a2a2309c658375e4f13d75459e70753609913734
pradanaadn <pradanaadn21@gmail.com>
2025-05-01 21:30:46 +0700

    Add starlight-theme-rapide dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
 		starlight({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [starlightThemeRapide()],
 			sidebar: [
 				{
 					label: 'Guides',
