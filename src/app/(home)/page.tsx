03a09ce50986f31511daeaea8ac6c3fe7958a9b9
pradanaadn <pradanaadn21@gmail.com>
2025-08-21 21:38:37 +0800

    feat: update homepage content and structure, add dynamic content page and layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,18 @@
 import Link from 'next/link';
 
+
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
+      <h1 className="text-7xl font-bold mb-4 font-sans max-w-[65%] mx-auto">
+        Welcome to My Personal Blog and Note 🥳
+      </h1>
+      <p className="mt-2 text-lg font-sans">
+        Contains my blog, tutorial, cheatsheet, and note
       </p>
+      <Link href="/content" className="mt-4 px-4 py-2 bg-fuchsia-400 font-bold font-sans text-white mx-auto rounded-xl hover:bg-fuchsia-600">
+        Get Started
+      </Link>
     </main>
   );
 }
